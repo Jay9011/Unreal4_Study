79602bccc210b5d3468bc307615dd3bcb52494d3
Jay9011 <whdtjq5904@gmail.com>
2023-02-16 12:50:32 +0900

    Delete U2011_03

--- U2211_03/Source/U2211_03/U2211_03.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "U2211_03.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, U2211_03, "U2211_03" );
